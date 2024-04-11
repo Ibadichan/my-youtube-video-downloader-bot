8b23662ffde2b56950261f47da68e0fcd8f55a54
Ivan Badicean <badican01117@gmail.com>
2024-04-12 01:35:32 +0300

    Use `webhookCallback` by default

--- index.js
+++ index.js
@@ -52,18 +52,12 @@ bot.command("download", async (ctx) => {
   }
 });
 
-// Start the server
-if (process.env.NODE_ENV === "production") {
-  // Use Webhooks for the production server
-  const app = express();
-  app.use(express.json());
-  app.use(webhookCallback(bot, "express"));
+const app = express();
+app.use(express.json());
+app.use(webhookCallback(bot, "express"));
 
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Bot listening on port ${PORT}`);
-  });
-} else {
-  // Use Long Polling for development
-  bot.start();
-}
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Bot listening on port ${PORT}`);
+});
